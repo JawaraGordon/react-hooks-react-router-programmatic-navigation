efdf9feab9c1377d177a6da748f03fbd25d6c3b3
JawaraGordon <jawaragordon@icloud.com>
2022-05-23 15:30:45 -0400

    completed assignment

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,12 @@
-import React from "react";
-import { Redirect } from "react-router-dom";
+import React from 'react';
+import { Redirect } from 'react-router-dom';
 
 function Home({ isLoggedIn }) {
   if (!isLoggedIn) return <Redirect to="/login" />;
 
   return (
     <div>
-      <h1>Home!</h1>
+      <h1>Welcome Home</h1>
     </div>
   );
 }
